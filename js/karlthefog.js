051f9fe910e13fe06f8c51d2cf55560fef929ab7
Aza Raskin <aza@air.local>
2015-07-30 00:55:10 -0700

    Wedding site!

--- js/karlthefog.js
+++ js/karlthefog.js
@@ -23,32 +23,6 @@ function StartKarl(){
   var mCanvas = document.getElementById('canvas'),
       mMouseIsDown = false
       mMouseOriX = 0, mMouseOriY = 0, mMousePosX = 0, mMousePosY = 0
-
-/*
-  document.onmousedown = function(ev){
-    mMouseOriX = (ev.pageX)*mCanvas.width/mCanvas.offsetWidth;
-    mMouseOriY = mCanvas.height - (ev.pageY)*mCanvas.height/mCanvas.offsetHeight;
-    mMouseIsDown = true;
-    return false
-  }
-  document.onmousemove = function(ev){
-    if( mMouseIsDown ){
-      mMousePosX = (ev.pageX)*mCanvas.width/mCanvas.offsetWidth;
-      mMousePosY = mCanvas.height - (ev.pageY)*mCanvas.height/mCanvas.offsetHeight;
-      mMousePosY = -80
-      console.log( mMousePosY)
-
-
-    }
-    return false
-  }
-
-  document.onmouseup = function(ev){
-    mMouseIsDown = false;
-    mMouseOriX = -Math.abs( mMouseOriX );
-    mMouseOriY = -Math.abs( mMouseOriY );
-  }*/
-
   
   glContext = createGlContext( document.getElementById('canvas') );
   
@@ -68,56 +42,6 @@ function StartKarl(){
   }
 
   render()
+  $('header').css('visibility', 'visible').addClass('wow fadeIn')
 
-  function snapToSection(){
-    var tops = $('.big,.section').map(function(){return $(this).position().top-window.scrollY})
-    tops = tops.sort(function(a,b){return a*a > b*b})
-
-    $('html, body').stop(true).animate({
-        scrollTop: tops[0]+window.scrollY
-    }, 150).dequeue()
-
-  }
-
-  var snapTimer = null
-  $(document).scroll(function(){
-    if( snapTimer ) clearTimeout(snapTimer)
-    snapTimer = setTimeout(function(){
-        snapToSection()
-    }, 300)
-  })
-
-  function save(){
-    var rsvp = {
-        name: $('#yourname').val(),
-        email: $('#youremail').val(),
-        people: $('#numpeople').val(),
-        thought: $('#thought').val(),
-        time: Date.now()
-    }
-
-    if( !rsvp.name || !rsvp.email || !rsvp.thought ){
-      return
-    }
-
-    var rsvpRef = new Firebase("https://toelsewhere.firebaseio.com/rsvp")
-    rsvpRef.push(rsvp)
-
-    $('.entry').fadeOut(1000)
-    $('<h1>').text("SEE YOU SOON. MEWRP.").hide().appendTo('.two .content').delay(1500).fadeIn(1000).delay(1500).fadeOut(2000, function(){
-        $('html, body').stop(true).animate({
-            scrollTop: 0
-        }, 8000).dequeue()
-    })
-
-    console.log("RSVPing")
-  }
-
-  $('.rsvp').click(function(){
-    save()
-  })
-
-  $(document).keyup(function(evt){
-    if( evt.which == 13 ){ save() }
-  })
 }
